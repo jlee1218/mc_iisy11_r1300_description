fe72fe1bcc7eb819e6505818a28daa88adb044e8
Jeffrey Lee <jeffrey1218@live.com>
2023-10-26 00:48:35 -0400

    working dummy model of iisy

--- launch/iisy11_model.launch.py
+++ launch/iisy11_model.launch.py
@@ -42,15 +42,15 @@ def generate_launch_description():
     default_value=default_rviz_config_path,
     description='Full path to the RVIZ config file to use')
      
-  # declare_use_joint_state_publisher_cmd = DeclareLaunchArgument(
-  #   name='gui',
-  #   default_value='True',
-  #   description='Flag to enable joint_state_publisher_gui')
+  declare_use_joint_state_publisher_cmd = DeclareLaunchArgument(
+    name='gui',
+    default_value='True',
+    description='Flag to enable joint_state_publisher_gui')
    
-  # declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
-  #   name='use_robot_state_pub',
-  #   default_value='True',
-  #   description='Whether to start the robot state publisher')
+  declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
+    name='use_robot_state_pub',
+    default_value='True',
+    description='Whether to start the robot state publisher')
  
   declare_use_rviz_cmd = DeclareLaunchArgument(
     name='use_rviz',
@@ -65,27 +65,27 @@ def generate_launch_description():
   # Specify the actions
  
   # Publish the joint state values for the non-fixed joints in the URDF file.
-  # start_joint_state_publisher_cmd = Node(
-  #   condition=UnlessCondition(gui),
-  #   package='joint_state_publisher',
-  #   executable='joint_state_publisher',
-  #   name='joint_state_publisher')
+  start_joint_state_publisher_cmd = Node(
+    condition=UnlessCondition(gui),
+    package='joint_state_publisher',
+    executable='joint_state_publisher',
+    name='joint_state_publisher')
  
-  # # A GUI to manipulate the joint state values
-  # start_joint_state_publisher_gui_node = Node(
-  #   condition=IfCondition(gui),
-  #   package='joint_state_publisher_gui',
-  #   executable='joint_state_publisher_gui',
-  #   name='joint_state_publisher_gui')
+  # A GUI to manipulate the joint state values
+  start_joint_state_publisher_gui_node = Node(
+    condition=IfCondition(gui),
+    package='joint_state_publisher_gui',
+    executable='joint_state_publisher_gui',
+    name='joint_state_publisher_gui')
  
-  # # Subscribe to the joint states of the robot, and publish the 3D pose of each link.
-  # start_robot_state_publisher_cmd = Node(
-  #   condition=IfCondition(use_robot_state_pub),
-  #   package='robot_state_publisher',
-  #   executable='robot_state_publisher',
-  #   parameters=[{'use_sim_time': use_sim_time, 
-  #   'robot_description': Command(['xacro ', urdf_model])}],
-  #   arguments=[default_urdf_model_path])
+  # Subscribe to the joint states of the robot, and publish the 3D pose of each link.
+  start_robot_state_publisher_cmd = Node(
+    condition=IfCondition(use_robot_state_pub),
+    package='robot_state_publisher',
+    executable='robot_state_publisher',
+    parameters=[{'use_sim_time': use_sim_time, 
+    'robot_description': Command(['xacro ', urdf_model])}],
+    arguments=[default_urdf_model_path])
  
   # Launch RViz
   start_rviz_cmd = Node(
@@ -102,15 +102,15 @@ def generate_launch_description():
   # Declare the launch options
   ld.add_action(declare_urdf_model_path_cmd)
   ld.add_action(declare_rviz_config_file_cmd)
-  # ld.add_action(declare_use_joint_state_publisher_cmd)
-  # ld.add_action(declare_use_robot_state_pub_cmd)  
+  ld.add_action(declare_use_joint_state_publisher_cmd)
+  ld.add_action(declare_use_robot_state_pub_cmd)  
   ld.add_action(declare_use_rviz_cmd) 
   ld.add_action(declare_use_sim_time_cmd)
  
   # Add any actions
-  # ld.add_action(start_joint_state_publisher_cmd)
-  # ld.add_action(start_joint_state_publisher_gui_node)
-  # ld.add_action(start_robot_state_publisher_cmd)
+  ld.add_action(start_joint_state_publisher_cmd)
+  ld.add_action(start_joint_state_publisher_gui_node)
+  ld.add_action(start_robot_state_publisher_cmd)
   ld.add_action(start_rviz_cmd)
  
   return ld
