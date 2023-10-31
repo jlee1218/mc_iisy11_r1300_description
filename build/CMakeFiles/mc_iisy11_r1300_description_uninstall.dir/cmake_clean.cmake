3aa83c36f35cf678b631348808b7a0bf589c0e41
Jeffrey Lee <jeffrey1218@live.com>
2023-10-31 18:39:23 -0400

    clean up

---
+++ build/CMakeFiles/mc_iisy11_r1300_description_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mc_iisy11_r1300_description_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/mc_iisy11_r1300_description_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mc_iisy11_r1300_description_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for mc_iisy11_r1300_description_uninstall.
+# This may be replaced when dependencies are built.
