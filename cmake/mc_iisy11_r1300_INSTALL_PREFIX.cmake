716d32426c1b750652634128b8b42fbd8d28485d
Jeffrey Lee <jeffrey1218@live.com>
2023-11-21 16:49:40 -0500

    module can now discover description

---
+++ cmake/mc_iisy11_r1300_INSTALL_PREFIX.cmake
@@ -0,0 +1,2 @@
+set(mc_iisy11_r1300_description_INSTALL_PREFIX "${mc_iisy11_r1300_description_DIR}/../")
+get_filename_component(mc_iisy11_r1300_description_INSTALL_PREFIX "${mc_iisy11_r1300_description_INSTALL_PREFIX}" REALPATH)
